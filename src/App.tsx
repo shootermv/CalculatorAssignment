ec3e04e71c2bcae1605062e609a812f3484aaa24
Moshe <shootermv@gmail.com>
2022-07-06 16:32:28 +0300

    add basic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./styles.css";
 import Header from "./header";
+import Operators from "./operators";
+import Buttons from "./buttons";
 export default function App() {
   return (
     <div className="App">
       <main>
         <Header />
-        <section>operators</section>
-        <section>buttons</section>
+        <Operators />
+        <Buttons />
       </main>
     </div>
   );
---
+++ src/operators/operators.tsx
@@ -0,0 +1,12 @@
+import "./operators.css";
+
+export default function Operators() {
+  return (
+    <section className="operators">
+      <button>+</button>
+      <button>-</button>
+      <button>*</button>
+      <button>/</button>
+    </section>
+  );
+}
